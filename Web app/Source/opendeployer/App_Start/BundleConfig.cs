f792d441b72b6a1eb807773c45607e2f07d2d403
Michael <mpbond@live.co.uk>
2016-04-22 13:36:22 +0100

    Base MVC template

---
+++ Web app/Source/opendeployer/App_Start/BundleConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace opendeployer
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/jquery-2.2.3.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/opendeployer.css"));
+        }
+    }
+}
